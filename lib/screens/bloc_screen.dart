d22f968c189fbadb04d94730e72e5e4dbab34f82
Jovanny <jovannycarlosgonzalez9@gmail.com>
2024-04-23 11:49:11 -0600

    add bloc screen

---
+++ lib/screens/bloc_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CubitScreen extends StatelessWidget {
+  const CubitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: ()=>{}, icon: Icon(Icons.refresh_rounded),)
+        ],
+        title: const Text('cubit counter'),
+      ),
+      body: const Center(
+        child: Text('cunter value: 2'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+              heroTag: 1, child: const Text('+3'), onPressed: () => {}),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+              heroTag: 2, child: const Text('+2'), onPressed: () => {}),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+              heroTag: 3, child: const Text('+1'), onPressed: () => {})
+        ],
+      ),
+    );
+  }
+}
