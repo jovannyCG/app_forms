1a0437b4043204eb42014d93076d0801784ac131
Jovanny <jovannycarlosgonzalez9@gmail.com>
2024-04-25 14:28:17 -0600

    desing form started

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,39 +1,57 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:forms_app/widgets/inputs/text_form_field.dart';
 
 class FormsScreen extends StatelessWidget {
   const FormsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text('registro'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('registro'),
+      ),
       body: const RegisterView(),
     );
   }
 }
+
 class RegisterView extends StatelessWidget {
   const RegisterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const FlutterLogo(size: 100,),
-              TextFormField(),
-              TextFormField(),
-              TextFormField(),
-               const SizedBox(height: 20,),
-              FilledButton.tonalIcon(onPressed: (){}, icon: const Icon(Icons.save), label: const Text('crear usuario'),),
-              const SizedBox(height: 20,)
-            ],
-          ),
-        ),)
-        );
+    return const SafeArea(
+        child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: SingleChildScrollView(child: Registerform()),
+    ));
+  }
+}
+
+class Registerform extends StatelessWidget {
+  const Registerform({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const FlutterLogo(
+          size: 100,
+        ),
+        InputField(),
+        const SizedBox(
+          height: 10,
+        ),
+         InputField(),
+        FilledButton.tonalIcon(
+          onPressed: () {},
+          icon: const Icon(Icons.save),
+          label: const Text('crear usuario'),
+        ),
+        const SizedBox(
+          height: 20,
+        )
+      ],
+    );
   }
 }
---
+++ lib/widgets/inputs/text_form_field.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+class InputField extends StatelessWidget {
+  const InputField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: (value){
+
+      },
+      validator: (value){
+        
+      },
+    );
+  }
+}
