d71cca00977297105ff356a880919aaefcf02702
Jovanny <jovannycarlosgonzalez9@gmail.com>
2024-04-25 12:49:39 -0600

    form added

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -8,8 +8,8 @@ class FormsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(title: Text('registro'),),
-      body: RegisterView(),
+      appBar: AppBar(title: const Text('registro'),),
+      body: const RegisterView(),
     );
   }
 }
@@ -18,6 +18,22 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const FlutterLogo(size: 100,),
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+               const SizedBox(height: 20,),
+              FilledButton.tonalIcon(onPressed: (){}, icon: const Icon(Icons.save), label: const Text('crear usuario'),),
+              const SizedBox(height: 20,)
+            ],
+          ),
+        ),)
+        );
   }
 }
